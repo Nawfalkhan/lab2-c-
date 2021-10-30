e98e13b001da045831c808984107115af04c5e5c
Nawfalkhan <92514487+Nawfalkhan@users.noreply.github.com>
2021-10-31 07:32:49 +0800

    change

---
+++ nav.php
@@ -0,0 +1,16 @@
+
+<img src="banner.png" alt="pess" align="center"  border="1">
+<meta charset="utf-8">
+
+
+<body>
+
+	
+<nav>
+ <a href="logcall.php">Log Call</a>
+ <a href="Update.php">Update</a>
+ <a href="#">Report</a>
+ <a href="#">History</a>
+ </nav>
+	
+</body>
